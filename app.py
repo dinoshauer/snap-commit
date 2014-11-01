ced4f3ffb9f276b6b6110f202575d76d2166af0b
Kasper Jacobsen <k@mackwerk.dk>
2014-11-01 18:21:00 +0100

    Added config stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import json
+from os import path
 
 import requests
 
@@ -7,28 +8,53 @@ import gitops
 import grab
 
 
+def load_config(config_path=None):
+    if config_path:
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+    else:
+        defaults = [
+            path.join(path.expanduser('~'), '.snap-commit.conf.json'),
+            '/etc/snap-commit/snap-commit.conf.json',
+            path.join(path.dirname(path.realpath(__file__)), 'defaults.json'),
+        ]
+        for config_path in defaults:
+            if path.isfile(config_path):
+                with open(config_path, 'r') as f:
+                    config = json.load(f)
+                    break
+    if '~' in config['image_dir']:
+        config['image_dir'] = config['image_dir'].replace(
+            '~',
+            path.expanduser('~')
+        )
+    return config
+
 def new_path(prefix=None):
     commit = gitops.get_commit()
-    user, repo = gitops.get_remote_url().split(':')[1].replace('.git', '').split('/')
-    filename = '{}_{}_{}_{}.jpg'.format(
+    remote = gitops.get_remote_url()
+    host = remote.split('.')[0].split('@')[1]
+    user, repo = remote.split(':')[1].replace('.git', '').split('/')
+    filename = '{}_{}_{}_{}_{}.jpg'.format(
         commit.commit_time*1000,
         commit.hex[:7],
         user,
-        repo
+        repo,
+        host
     )
     if prefix:
         paths = [prefix, filename]
     else:
         paths = [filename]
-    return os.path.join(*paths)
+    return path.join(*paths)
 
-def upload():
-    base = '/home/k/.snap-commit'
+def upload(config):
+    base = config['image_dir']
     for item in os.listdir(base):
         try:
-            filepath = os.path.join(base, item)
+            filepath = path.join(base, item)
             r = requests.post(
-                'http://0.0.0.0:5000/v1',
+                config['snap_server'] + '/v1',
                 files={'file': (item, open(filepath, 'rb'))}
             )
             if r.ok:
@@ -39,18 +65,28 @@ def upload():
             print 'error occurred', e
             print 'saving image for next upload'
 
-def run_hook():
-    path = new_path('/home/k/.snap-commit')
+def run_hook(config):
+    file_path = new_path(config['image_dir'])
     try:
-        os.makedirs(os.path.dirname(path))
+        os.makedirs(path.dirname(file_path))
     except OSError, e:
         if e.errno is not 17:
             raise
-    grab.grab_image(path)
+    grab.grab_image(
+        file_path,
+        width=config['width'],
+        height=config['height'],
+        device=config['video_device']
+    )
 
 
 if __name__ == '__main__':
     import sys
-    run_hook()
-    upload()
+    if len(sys.argv) > 1:
+        config = load_config(sys.argv[1])
+    else:
+        config = load_config()
+    run_hook(config)
+    if config.get('snap_server') and config['snap_server']:
+        upload(config)
     sys.exit(0)
--- gitops.py
+++ gitops.py
@@ -5,10 +5,7 @@ REPO = pygit2.Repository('.git')
 
 def get_commit():
     return REPO.revparse_single('HEAD')
-    # rev.hex[:7], rev.commit_time*1000
 
 def get_remote_url():
     for remote in REPO.remotes:
-        if 'github' in remote.url:
-            return remote.url
-            # .split(':')[1].replace('.git', '').split('/')
+        return remote.url

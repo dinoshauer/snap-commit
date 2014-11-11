b4927aa5b7e7b11a08290f01e05e5b5fcf6061cd
Kasper Jacobsen <k@mackwerk.dk>
2014-11-11 18:37:05 +0100

    Added setup.cfg, setup.py, added variables and doctstring to snapcommit

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import snapcommit
+
+setup(
+    name=snapcommit.name,
+    version=snapcommit.version,
+    author='Kasper Jacobsen',
+    author_email='k@mackwerk.dk',
+    description=snapcommit.__doc__,
+    url=snapcommit.project_url,
+    license='MIT',
+    keywords='',
+    packages=[
+        'snapcommit'
+    ],
+    entry_points={
+        'console_scripts': [
+            'snapcommit = snapcommit.cli:main',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ],
+    install_requires=[
+        'Pillow',
+        'click',
+        'pygit2',
+        'requests',
+        'v4l2',
+        'v4l2capture',
+    ],
+)
---
+++ snapcommit/__init__.py
@@ -0,0 +1,6 @@
+'''Snapcommit is a (lolcommits clone http://mroth.github.io/lolcommits/)
+git post-commit hook, that will take a picture of you after every commit.'''
+
+name = 'snap-commit'
+version = '0.0.1'
+project_url = 'https://github.com/Dinoshauer/snap-commit'

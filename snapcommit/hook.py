b885e95abdf7e67d7c559350a5828d8e767b7848
Kasper Jacobsen <k@mackwerk.dk>
2014-11-14 20:28:42 +0100

    Move defaults into variable in hooks.py

--- snapcommit/hook.py
+++ snapcommit/hook.py
@@ -10,7 +10,19 @@ import gitops
 import grab
 
 
+DEFAULTS = {
+    'image_dir': '~/.snap-commits/snaps/',
+    'video_device': '/dev/video0',
+    'width': 640,
+    'height': 480,
+    'snap_server': 'http://0.0.0.0:5000',
+    'log_dir': '~/.snap-commits/logs/',
+    'log_level': 'info',
+    'keep_snaps': False
+}
+
 def load_config(config_path=None):
+    config = {}
     print path.join(path.dirname(path.realpath(__file__)), 'defaults.json')
     if config_path:
         with open(config_path, 'r') as f:
@@ -26,6 +38,7 @@ def load_config(config_path=None):
                 with open(config_path, 'r') as f:
                     config = json.load(f)
                     break
+    config = dict(DEFAULTS.items() + config.items())
     if '~' in config['image_dir']:
         config['image_dir'] = config['image_dir'].replace(
             '~',
